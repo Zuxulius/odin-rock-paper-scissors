a7152045a28720f7b55e0657b09ec5328175dd12
Axel <70969800+Zuxulius@users.noreply.github.com>
2023-02-19 22:12:49 +0100

    create playRound function

--- r_p_s.js
+++ r_p_s.js
@@ -8,6 +8,31 @@
 const choices = ["Rock","Paper","Scissors"];
 
 function getComputerChoice() {
-    let choice = Math.floor(Math.random() * choices.length)
-    console.log(choice)
+    // Math.random gives number between 0 and 1, not including 1
+    // Multiplying that by the length of the array gives you any number
+    // between 0 and length of array.
+    // Flooring it gives you an integer between 0(inclusive)
+    // and the length of the array (non inclusive)
+    let choice = Math.floor(Math.random() * choices.length);
+    return choices[choice];
 }
+
+function playRound(player_selection, computerSelection) {
+    if ((player_selection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "rock") ||
+    (player_selection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "paper") ||
+    (player_selection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "scissors")) {
+        return "It's a tie!";
+
+    } else if ((player_selection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") ||
+    (player_selection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") ||
+    (player_selection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper")) {
+        return "You won!";
+
+    } else {
+        return "You lost!";
+    }
+}
+
+const player_selection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(player_selection, computerSelection));
